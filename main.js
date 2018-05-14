6ae964234bbd60a2a8250a4a7bdb6b6448f902a2
Allan Tenenbaum <allan@Allans-MacBook-Pro.local>
2018-05-13 22:45:30 -0400

    finished

---
+++ main.js
@@ -0,0 +1,61 @@
+//initialize variables
+var http = require("http");
+var fs = require("fs");
+vowels = ["a","e","i","o","u","y"];
+pigEnding = "ay";
+removedLetters = "";
+newWord = "";
+newSentence = "";
+storeConsonants = [];
+newSentenceArray = [];
+
+//checks each letter of the word and identifies if it is a vowel or a consonant
+function letterCheck(){
+    storeConsonants = [];
+    for(i = 0; i < splitWord.length;i++){
+        //if it is a vowel
+        if (vowels.indexOf(splitWord[i]) > -1){
+            break;
+        }
+        //if it is a consonant
+        else if (vowels.indexOf(splitWord[i]) == -1){
+            storeConsonants.push(splitWord[i]);
+        }
+    }
+    removeConsonants();
+}
+function removeConsonants(){
+    for (iii = 0; iii < storeConsonants.length;iii++){
+        index = splitWord.indexOf(storeConsonants[iii]);
+        splitWord.splice(index,1);
+    }
+    translate();
+}
+function translate(){
+    joinedLetters = storeConsonants.join("");
+    joinedSplitWord = splitWord.join("");
+    newWord = joinedSplitWord + joinedLetters + pigEnding + " ";
+    newSentenceArray.push(newWord);
+}
+function makeNewSentence(){
+    newSentence = newSentenceArray.join("");
+} 
+//reads file
+fs.readFile("eng.txt", "utf8", function (err, data){
+    if(err) throw err;
+    else{
+        inputText = data.toString().split("\n");
+        for(ii = 0; ii < inputText.length; ii++){
+            splitWord = inputText[ii].split("");
+            letterCheck();
+        }
+        makeNewSentence();
+        console.log(newSentence);
+
+    }
+}); 
+fs.writeFile("pig.txt", newSentence, function(err) {
+    if(err) throw err;
+
+    console.log("Exported new sentence to pig.txt");
+});
